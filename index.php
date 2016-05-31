db6a1a78a11dc4d9a10e108dac4fbbe55438aaca
Milind Chavan <milind.chavan@infobeans.com>
2016-05-31 15:37:32 +0530

    updated

--- index.php
+++ index.php
@@ -12,6 +12,6 @@
       </tr>
     </table>
 
-    <p>This is the home page for <b>Deployment Automation</b>.</p>
+    <p>This is the home page for <b>Deployment Automation</b>. This is test</p>
   </body>
 </html>

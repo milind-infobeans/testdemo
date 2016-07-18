08ba70d3e9d506c4bc10650c3225d73e2f68d2fb
Milind Chavan <milind.chavan@infobeans.com>
2016-07-18 10:21:59 +0530

    index file added.

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <title>Demo Application</title>
+  </head>
+  <body bgcolor=white>
+
+    <table border="0" cellpadding="10">
+      <tr>
+        <td>
+          <h1>Sample Deployment Automation Application</h1>
+        </td>
+      </tr>
+    </table>
+
+    <p>This is the home page for <b>Deployment Automation</b>. <i>this is test </i></p>
+<p>This is the test page.</p>
+  </body>
+</html>

ee4b387b69e96969bf749fc05021fefa7b04e18d
Milind Chavan <milind.chavan@infobeans.com>
2016-07-18 11:07:27 +0530

    index file updated.

--- index.php
+++ index.php
@@ -13,6 +13,6 @@
     </table>
 
     <p>This is the home page for <b>Deployment Automation</b>. <i>this is test </i></p>
-    <p>This is the test page please <i>ignore</i> it.</p>
+    <p>This is the test page please <i>ignore...</i> it.</p>
   </body>
 </html>
